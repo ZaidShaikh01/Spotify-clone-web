14d1d30318a7de47c26653fe280e18b4b6412e73
ZaidShaikh <szaid5146@gmail.com>
2026-03-23 16:35:58 +0530

    Search bar implementaion completed

--- js/search.js
+++ js/search.js
@@ -1,5 +1,3 @@
-const searchContainer = document.querySelector('.search-container');
-
 /*
 
 Logic for implementing the local storage
@@ -12,6 +10,43 @@ destructure the elements (Done)
 
 */
 
+// Inserting some valus in local storage
+localStorage.setItem('Shape of You', 'Ed Sheeran');
+localStorage.setItem('Blinding Lights', 'The Weeknd');
+localStorage.setItem('Believer', 'Imagine Dragons');
+localStorage.setItem('Rolling in the Deep', 'Adele');
+localStorage.setItem('Smells Like Teen Spirit', 'Nirvana');
+localStorage.setItem('Bohemian Rhapsody', 'Queen');
+localStorage.setItem('Lose Yourself', 'Eminem');
+localStorage.setItem('Hotel California', 'Eagles');
+localStorage.setItem('Someone Like You', 'Adele');
+localStorage.setItem('Uptown Funk', 'Mark Ronson ft. Bruno Mars');
+localStorage.setItem('Tum Hi Ho', 'Arijit Singh');
+localStorage.setItem('Kabira', 'Tochi Raina, Rekha Bhardwaj');
+localStorage.setItem('Kal Ho Naa Ho', 'Sonu Nigam');
+localStorage.setItem('Tera Ban Jaunga', 'Akhil, Tulsi Kumar');
+localStorage.setItem('Chaiyya Chaiyya', 'Sukhwinder Singh, Sapna Awasthi');
+localStorage.setItem('Pee Loon', 'Mohit Chauhan');
+localStorage.setItem('Tujh Mein Rab Dikhta Hai', 'Roop Kumar Rathod');
+localStorage.setItem('Ae Dil Hai Mushkil', 'Arijit Singh');
+localStorage.setItem('Galliyan', 'Ankit Tiwari');
+localStorage.setItem('Dil Diyan Gallan', 'Atif Aslam');
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    showAllEntries();
+  },
+  { once: true },
+);
+
+const inputField = document.querySelector('.header-input');
+
+inputField.addEventListener('input', function () {
+  search(inputField.value);
+  console.log(inputField.value);
+});
+
 function createNewTableItem(song, author) {
   const tr = document.createElement('tr');
   const tdSong = document.createElement('td');
@@ -21,13 +56,35 @@ function createNewTableItem(song, author) {
   tr.append(tdSong, tdAuthor);
   document.querySelector('.search-body').appendChild(tr);
 }
-for (let i = 0; i < localStorage.length - 1; i++) {
-  const songName = localStorage.key(i);
-  const author = localStorage.getItem(songName);
+function removeAllEntries() {
+  document.querySelector('.search-body').innerHTML = '';
+}
+function showAllEntries() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const songName = localStorage.key(i);
+    const author = localStorage.getItem(songName);
 
-  if (songName.includes('@gmail.com')) {
-    continue;
-  } else {
+    if (songName.includes('@gmail.com')) {
+      continue;
+    }
     createNewTableItem(songName, author);
+  }
+}
+function search(inputValue) {
+  removeAllEntries();
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const songName = localStorage.key(i);
+    const author = localStorage.getItem(songName);
+    console.log(inputValue);
+    console.log(songName);
+
+    if (songName.includes('@gmail.com')) {
+      // console.log(songName);
+      continue;
+    }
+    if (songName.toLowerCase().includes(inputValue.toLowerCase())) {
+      createNewTableItem(songName, author);
+    }
   }
 }
